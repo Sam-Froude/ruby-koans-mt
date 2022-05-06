58a32dc72726773f7588bb3d8c048a736bc39202
Tarik Merrlees <tarik.merrylees@madetech.com>
2022-05-06 12:38:16 +0100

    started to create string koans

---
+++ spec/02_strings_spec.rb
@@ -0,0 +1,21 @@
+require "rspec"
+
+# Let these be your first humble steps towards learning a new language.
+
+RSpec.describe "Strings: " do
+
+ # replace ___ with the correct answer to make the tests pass
+
+    it "Strings are there to represent text" do 
+        string = "madetech"
+        expect(__).to be_a_kind_of(String) 
+    end
+
+    it "Strings can be represented in either double or single quotes" do 
+        expect("madetech").to eq(___)
+    end
+
+
+end
+
+    
