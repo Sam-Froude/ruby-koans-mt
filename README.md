cd611418aac2b94aa8a145798cd12fbeb489225e
Tarik Merrlees <tarik.merrylees@madetech.com>
2022-05-13 14:15:02 +0100

    add command in readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ruby-koans-mt
 New koans for Ruby
+
+Run Koans: `bundle exec guard`
--- spec/example_spec.rb
+++
@@ -1,6 +0,0 @@
-RSpec.describe "my group" do
-   
-    it "We shall contemplate truth by testing reality, via equality" do 
-        expect(true).to eq(false)
-    end
-  end
